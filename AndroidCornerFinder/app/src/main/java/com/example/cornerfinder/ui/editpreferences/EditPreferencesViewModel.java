91af04016f388de303ab728cdec613f9925406e4
Paulo González Chorén <pgonzalezc22@fpcoruna.afundacion.org>
2024-01-17 09:41:17 +0100

    #13 Cambiado el nombre que se muestra en la pantalla al castellano

--- AndroidCornerFinder/app/src/main/java/com/example/cornerfinder/ui/editpreferences/EditPreferencesViewModel.java
+++ AndroidCornerFinder/app/src/main/java/com/example/cornerfinder/ui/editpreferences/EditPreferencesViewModel.java
@@ -10,7 +10,7 @@ public class EditPreferencesViewModel extends ViewModel {
 
     public EditPreferencesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Preferences");
+        mText.setValue("Preferencias");
     }
 
     public LiveData<String> getText() {
