8479434282526386f3b29593927a9cd715ed83be
Bl4nc018 <abr04work@gmail.com>
2024-01-23 17:28:49 +0100

    Creado y rellenado el vuelo View Holder.

--- AndroidCornerFinder/app/src/main/java/com/example/cornerfinder/summermode/SummerModeViewHolder.java
+++ AndroidCornerFinder/app/src/main/java/com/example/cornerfinder/summermode/SummerModeViewHolder.java
@@ -1,4 +1,54 @@
 package com.example.cornerfinder.summermode;
 
-public class SummerModeViewHolder {
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cornerfinder.R;
+
+public class SummerModeViewHolder extends RecyclerView.ViewHolder {
+    private TextView placeName;
+    private TextView etiqueta;
+    private TextView descripcion;
+    private ImageView imageView;
+    private Button location;
+
+
+    public SummerModeViewHolder(@NonNull View ivi){
+        super(ivi);
+        placeName = ivi.findViewById(R.id.placeName);
+        etiqueta = ivi.findViewById(R.id.etiqueta);
+        descripcion = ivi.findViewById(R.id.descripcion);
+        imageView = ivi.findViewById(R.id.image_view);
+        location = ivi.findViewById(R.id.location);
+    }
+
+
+    public void showData(SummerModeData summerData){
+        placeName.setText(summerData.getPlaceName());
+        descripcion.setText(summerData.getDescripcion());
+        etiqueta.setText(summerData.getEtiqueta());
+        Util.downloadBitmapToImageView(summerData.getImage_url(), this.imageView);
+
+
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Funciona el botón");
+            }
+        });
+
+
+
+
+
+    }
+
 }
